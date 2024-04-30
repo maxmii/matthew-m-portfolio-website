638f5805e8e21eb633dac309caaace34d8066323
maxamills <morrm046@gmail.com>
2024-04-30 18:14:27 +0100

    Generic Website for now and is under construction

---
+++ src/pages/constructionPage.js
@@ -0,0 +1,9 @@
+const ConstructionPage = () => {
+  return (
+    <div className="content-center">
+      <h4 className="text-2xl">This website is currently under construction</h4>
+    </div>
+  )
+}
+
+export default ConstructionPage
